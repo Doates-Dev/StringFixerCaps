c822a55d2139d2f0f9fae6436b20d49f6b45f5fa
Doates <Doatesndmore@hotmail.com>
2026-09-24 21:04:22 +0100

    First

---
+++ CapitalStringFix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Console.Write("Enter some text: ");
+        string text = Console.ReadLine() ?? "";
+
+        string fixedText = FixCapitalization(text);
+
+        Console.WriteLine("Fixed text: " + fixedText);
+
+        Clipboard.SetText(fixedText);
+
+        Console.WriteLine();
+        Console.WriteLine("Fixed text copied to clipboard!");
+        
+    }
+
+    static string FixCapitalization(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+
+        input = input.ToLower();
+
+        return char.ToUpper(input[0]) + input.Substring(1);
+    }
+}
